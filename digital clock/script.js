95e89f0a23a4c62194c1624e5925879ad72519e6
tanuja-rupnawar <141419781+tanuja-rupnawar@users.noreply.github.com>
2024-01-12 23:08:03 +0530

    Add files via upload

---
+++ digital clock/script.js
@@ -0,0 +1,24 @@
+function digitalClock(){
+    let dateFunction = new Date()
+    let day = dateFunction.getDay()
+    let hours = dateFunction.getHours()
+    let minutes = dateFunction.getMinutes()
+    let seconds = dateFunction.getSeconds()
+    let timeFormat = 'AM'
+
+    timeFormat = hours >= 12 ? 'PM' : 'AM'
+    hours = hours == 0 ? 12 : hours
+    hours = hours > 12 ? hours - 12 : hours
+    hours = hours < 10 ? '0' + hours : hours
+    minutes = minutes < 10 ? '0' + minutes : minutes
+    seconds = seconds < 10 ? '0' + seconds : seconds
+
+    document.querySelector('.hours').innerHTML = hours
+    document.querySelector('.minutes').innerHTML = minutes
+    document.querySelector('.seconds').innerHTML = seconds
+    document.querySelector('.format').innerHTML = timeFormat
+
+    let todaysDay = document.querySelector(`.weekdays :nth-child(${day + 1})`)
+    todaysDay.classList.add('active')
+}
+setInterval(digitalClock, 1000)
